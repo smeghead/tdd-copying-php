62162a3db0312ee24bda3ce6f7ec4162d7ff36ec
smeghead <smeghead7@gmail.com>
2023-03-18 15:28:06 +0900

    P12 第1章完了

--- src/Dollar.php
+++ src/Dollar.php
@@ -6,11 +6,13 @@ namespace Smeghead\TddCopyingPhp;
 
 class Dollar
 {
-    public int $amount = 10;
+    public int $amount = 0;
     public function __construct(int $amount)
     {
+        $this->amount = $amount;
     }
     public function times(int $multiplier): void
     {
+        $this->amount *= $multiplier;
     }
 }
